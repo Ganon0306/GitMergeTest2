7d262f41ba24014635009f902197e054585c0757
Ganon0306 <134994629+Ganon0306@users.noreply.github.com>
2023-07-03 15:01:44 +0900

    1

--- GitMergeTest/Program.cs
+++ GitMergeTest/Program.cs
@@ -6,32 +6,29 @@ using System.Threading.Tasks;
 
 namespace GitMergeTest
 {
-    internal class Program
+    public class Program
     {
-        public class Program
+        static void Main(string[] args)
         {
-            static void Main(string[] args)
-            {
-                Ruler ruler = new Ruler(10);
-                ruler.Run();
-            }
+            Ruler ruler = new Ruler(10);
+            ruler.Run();
+        }
+    }
+
+    public class Ruler
+    {
+        private const float ONE_INCH = 2.54F;
+        public int Centimeter { get; set; } = 0;
+        public float Inch
+        {
+            get { return Centimeter * ONE_INCH; }
+            private set { Centimeter = (int)(value / ONE_INCH); }
         }
 
-        public class Ruler
+        public Ruler(int cmValue) { Centimeter = cmValue; }
+        public void Run()
         {
-            private const float ONE_INCH = 2.54F;
-            public int Centimeter { get; set; } = 0;
-            public float Inch
-            {
-                get { return Centimeter * ONE_INCH; }
-                private set { Centimeter = (int)(value / ONE_INCH); }
-            }
-
-            public Ruler(int cmValue) { Centimeter = cmValue; }
-            public void Run()
-            {
-                Console.WriteLine($"{this.Centimeter}cm는 {this.Inch}inch 입니다.");
-            }
+            Console.WriteLine($"{this.Centimeter}cm는 {this.Inch}inch 입니다.");
         }
     }
 }
